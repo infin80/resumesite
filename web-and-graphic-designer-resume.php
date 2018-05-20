4ddb15134049cc4a5ebe620c9c0c927108415bfb
infin80 creative <design@infin80.com>
2018-05-20 12:39:23 -0700

    Add files via upload

---
+++ web-and-graphic-designer-resume.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Resume of David Grant - Graphic/Web Designer &amp; Marketing Strategist</title>
+  <meta name="description" content="David Grant CV: Las Vegas logo and graphic designer, responsive SEO web developer and brand marketing pro with 20 years of experience." />
+  <link rel='shortcut icon' type='image/x-icon' href='favicon.ico' />
+  <link rel="shortcut icon" href="/favicon.ico?v=2">
+
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href="//fonts.googleapis.com/css?family=Abel%7CStint+Ultra+Condensed%7CMontserrat:400,700%7CMichroma" rel="stylesheet">
+
+  <link href="css/style.css" rel="stylesheet">
+  <!-- <link rel="stylesheet" href="min/style.min.css"> -->
+
+  <!-- <script src="full/PluginDetect.js"></script> -->
+</head>
+<body>
+
+
+<div class="interactive-resume-wrap">
+    <!-- <script>
+    var options = {
+       fallbackLink: '<p>This browser does not support inline PDFs. Please download the PDF to view it: <a href="https://goo.gl/6hjftA">Download Interactive PDF</a></p>'};
+    PDFObject.embed("http://resume.infin80.com/DG-2015IA.pdf", ".pdf-container", options);
+    </script> -->
+    <object data="http://resume.infin80.com/DG-2015IA.pdf"
+            type="application/pdf"
+            width="1000px"
+            height="1500px"
+            background-color="none">
+    <p>This browser does not support inline PDFs. Please download the PDF to view it: <a href="https://goo.gl/6hjftA">Download Interactive PDF</a></p>
+    </object>
+
+<br><br>
+<p>PDF viewer plugins are not fully supported, much to my <a href="http://resume.infin80.com/index.php#contact">dismay and regret</a>. Please refresh and wait 20 seconds before giving up. Thank you for your patience.</p>
+  <div class="center">
+    <a class="button lightbg-orange hvr-pulse clearfix" href="https://goo.gl/58ALzr"><span>View Printer-Friendly Resume</span><div class="icon fa fa-download"></div></a>
+  </div>
+  <br>
+  <div class="copyright">
+  <small>&copy;<?php echo date("Y") ?> infin80 creative&reg; | Web &amp; Graphic Design. All rights reserved under International Copyright Conventions. No part of this website may be reproduced, replicated, reiterated, duplicated, conduplicated, retyped, read aloud and recorded (on audio tape, disc or any other advanced technology), lipsynched, stored in a retrieval system, archived or transmitted in any form or by any means, including chemical, mechanical, genetic, optical, xerographic, holographic, electronic, stereophonic, ceramic, telepathic or transcribed by hand (manuscript or cursive).</small>
+   <small class="copyright">Aside from the previously mentioned methods, copying IS permitted via Sections 107 and 108 of U.S. Copyright Law. An exception may be permitted for potential employers and reviewers who publicly publish positive posts promising to read in its entirety this verbose resume before writing their raving review. Prior written permission from the Publisher of this content is required. Not valid in New Jersey.</small>
+   </div>
+</div>
+<br>
+
+
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+<!-- <script src="https://code.jquery.com/jquery-2.2.1.min.js" integrity="sha256-gvQgAFzTH6trSrAWoH1iPo9Xc96QxSZ3feW6kem+O00=" crossorigin="anonymous"></script> -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <!-- <script src="full/modernizr.custom.68424.js" charset="utf-8" async defer></script> -->
+  <!-- <script src="js/pdfobject.min.js"></script> -->
+
+  <script>
+    var clicky_site_ids = clicky_site_ids || [];
+    clicky_site_ids.push(66391169);
+    (function() {
+      var s = document.createElement('script');
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = '//static.getclicky.com/js';
+      ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+    })();
+  </script>
+  </body>
+</html>
